529c51404d4626d148e26e677663436037048786
kpalaciosm02 <kpalaciosm02@gmail.com>
2022-03-28 22:34:29 -0600

    Add project files.

---
+++ Tarea Programada 2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tarea_Programada_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
